7b5c7a8f462a0c9dd842ddabc9ffa0dc8349ff40
pskanade <k.pranav.apk@gmail.com>
2019-01-17 17:53:05 +0530

    Added allowed hosts and corrected the invocation cmd

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^&b8@)^ufv411w!i7j68^!mu1htw&a_g@-0!0nxqc37yo@lgrl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.53.20.144']
 
 
 # Application definition
