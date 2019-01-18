7d29cc815d85220fab2c01395227764cd247b0e9
pskanade <k.pranav.apk@gmail.com>
2019-01-18 15:54:45 +0530

    Added views and serializers for getting auth token

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
@@ -16,3 +17,31 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+
+    email = serializers.CharField()
+    password = serializers.CharField(
+            style={'input_type': 'password'},
+            trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+                request=self.context.get('request'),
+                username=email,
+                password=password
+        )
+
+        if not user:
+            msg = ("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code="authentication")
+
+        attrs['user'] = user
+        return attrs
