f4ba2a34fd6e710fbd5b42142e60a1416ce6da32
pskanade <k.pranav.apk@gmail.com>
2019-01-19 09:15:22 +0530

    Addded tags create api

--- recipe/tests/test_tags_api.py
+++ recipe/tests/test_tags_api.py
@@ -68,3 +68,25 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_success(self):
+        """Test creating a new tag"""
+        payload= {
+            'name': 'test tag'
+        }
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.all().filter(
+            user=self.user1,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating invalid tag"""
+        payload = {'name': ""}
+        res = self.client.post(payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
